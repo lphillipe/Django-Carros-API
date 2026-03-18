f8f3cf400cb3a3437c9379ac3efcb61cb4e5dfdb
lphillipe <luisphillipe200@gmail.com>
2026-03-18 07:13:07 -0300

    config: views render

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def cars_view(request):
-    
-    return HttpResponse(html)
+    return render(request, 'cars.html')
