1f343a8acfe3e5b34bf1d3fd4a0139b146ebede0
lphillipe <luisphillipe200@gmail.com>
2026-03-18 07:25:36 -0300

    config views render

--- cars/views.py
+++ cars/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def cars_view(request):
-    return render(request, 'cars.html')
+    return render(
+        request,
+          'cars.html',
+          {'cars': {'model': 'Astra 2.0'} }
+    )
