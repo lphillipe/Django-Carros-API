abfb1a4b024537b2ebf0c6a18983177b10691929
lphillipe <luisphillipe200@gmail.com>
2026-03-19 07:17:07 -0300

    config views filter search

--- cars/views.py
+++ cars/views.py
@@ -4,6 +4,10 @@ from cars.models import Car
 # Create your views here.
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+    
+    if search:
+        cars = cars.filter(model__contains=search)
     
     
     return render(
