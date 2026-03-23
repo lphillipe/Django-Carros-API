2e42d22be5241ee24c9e1dd6de97346b21cf2c14
lphillipe <luisphillipe200@gmail.com>
2026-03-23 06:58:06 -0300

    Update forms and setup cars

--- cars/forms.py
+++ cars/forms.py
@@ -5,3 +5,10 @@ class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = '__all__'
+
+
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor mínimo do carro deve ser de R$20.000')
+        return value
