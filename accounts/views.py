5a32049d543a0a046f94af1e9e9387a81d34cdc9
lphillipe <luisphillipe200@gmail.com>
2026-03-24 07:35:36 -0300

    Implement login functionality with authentication and form handling

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -14,5 +15,15 @@ def register_view(request):
     return render(request, 'register.html', {'user_form': user_form})
 
 def login_view(request):
-    login_form = AuthenticationForm()
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
     return render(request,'login.html', {'login_form': login_form})
