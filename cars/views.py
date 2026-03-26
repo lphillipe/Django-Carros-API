58149ca39098d4db9b110e05ad7b11f34c01b1f7
lphillipe <luisphillipe200@gmail.com>
2026-03-26 07:17:26 -0300

    Refactor cars views to implement CarsListView and update URL patterns

--- cars/views.py
+++ cars/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -19,6 +20,19 @@ class CarsView(View):
             'cars.html',
             {'cars': cars}
         )
+    
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+
+        if search:
+            cars = cars.filter(model_icontains=search)
+        return cars
 
 class NewCarView(View):
 
