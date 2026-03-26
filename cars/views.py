d39463c72748560b133a69be41894e88b9d6cb4f
lphillipe <luisphillipe200@gmail.com>
2026-03-26 06:37:43 -0300

    Refactor cars view to use class-based view and update URL pattern

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 # Create your views here.
 def cars_view(request):
@@ -15,6 +16,22 @@ def cars_view(request):
         'cars.html',
         {'cars': cars }
     )
+
+class CarsView(View):
+
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            cars = cars.filter(model__icontains=search)
+
+        return render(
+            request,
+            'cars.html',
+            {'cars': cars}
+        )
+
 
 def new_car_view(request):
     if request.method == 'POST':
